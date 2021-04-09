9f326f3b221b3f0005029996c5e0706bccea6bb9
rsconklin <81965800+rsconklin@users.noreply.github.com>
2021-04-09 15:26:01 -0400

    Update HackerRank.py

--- HackerRank.py
+++ HackerRank.py
@@ -1,4 +1,41 @@
+## HR: Merge the Tools!. https://www.hackerrank.com/challenges/merge-the-tools/problem. Type: Strings. Date = 4/09/21.
+
+def merge_the_tools(string, k):
+
+    n = len(string)
+    m = int(n/k)
+    
+    for i in range(1, m + 1):
+
+        udup = list(string[k*(i-1):k*i])
+        ui = udup[0]
+        uset = set(ui)
+        
+        for j in range(1, len(udup)):
+            if udup[j] in uset:
+                pass
+            else:
+                ui += udup[j]
+                uset.add(udup[j])
+        print(ui)
+
 ## LeetCode (LC): Two Sum II. https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/. Type: Arrays. Date = 4/09/21
+
+# From LC fastest solution.
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        
+        left = 0
+        right = len(numbers) - 1
+        
+        while left != right:
+            total = numbers[left] + numbers[right]
+            if total == target:
+                return [left + 1, right + 1]
+            elif total > target:
+                right -= 1
+            else:
+                left += 1
 
 # Faster, because of skipping the inner loop if a duplicate in 'i' is found. But still slow compared to other users.
 class Solution:
