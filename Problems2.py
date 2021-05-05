06f4a89eacae2e4e4c2b590b27ee0eb0541c14c8
rsconklin <81965800+rsconklin@users.noreply.github.com>
2021-05-05 18:06:10 -0400

    Update Problems2.py

--- Problems2.py
+++ Problems2.py
@@ -1,3 +1,57 @@
+## LC: LRU Cahe. https://leetcode.com/problems/lru-cache/. Type: Data Structures. Date: 5/05/21.
+# O(1) for put() and get(). Doubly-linked list.
+class Node:
+    '''For a doubly-linked list with dictionary attributes.'''
+    
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._add(node)
+            return node.val
+        else:
+            return -1
+            
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self._remove(self.cache[key])
+        node = Node(key, value)
+        self.cache[key] = node
+        self._add(node)
+        if len(self.cache) > self.capacity:
+            node = self.head.next
+            self._remove(node)
+            del self.cache[node.key]
+    
+    def _remove(self, node):
+        p = node.prev
+        n = node.next
+        p.next = n
+        n.prev = p
+        
+    def _add(self, node):
+        p = self.tail.prev
+        p.next = node
+        self.tail.prev = node        
+        node.prev = p
+        node.next = self.tail
+
 ## LC: Reorder Data in Log Files. https://leetcode.com/problems/reorder-data-in-log-files/. Type: Strings, Sort. Date: 5/05/21.
 # O(k * nlogn)
 class Solution:
