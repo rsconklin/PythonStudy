97697ff674689391c8b26ffcf23f9bab799a336d
rsconklin <81965800+rsconklin@users.noreply.github.com>
2021-05-26 20:12:42 -0400

    Update Problems3.py

--- Problems3.py
+++ Problems3.py
@@ -1,4 +1,4 @@
-## LC: Find Anagram Mappings.
+## LC: Find Anagram Mappings. https://leetcode.com/problems/find-anagram-mappings/. Type: Arrays. Date: 5/26/21.
 # O(n^2)
 class Solution:
     def anagramMappings(self, nums1: List[int], nums2: List[int]) -> List[int]:
