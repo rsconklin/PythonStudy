14c589f4ac9c6b02c322619733a59d9564ed3b85
rsconklin <81965800+rsconklin@users.noreply.github.com>
2021-05-26 20:12:20 -0400

    Update Problems3.py

--- Problems3.py
+++ Problems3.py
@@ -1,1 +1,11 @@
-
+## LC: Find Anagram Mappings.
+# O(n^2)
+class Solution:
+    def anagramMappings(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        mapping = []
+        
+        for n in nums1:
+            mapping.append(nums2.index(n))
+        
+        return mapping
